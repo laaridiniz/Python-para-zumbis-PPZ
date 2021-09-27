e2d2343b6be31a3ba215536a4cb07f157d185b7d
Larissa Diniz <diniz.laari@gmail.com>
2021-09-27 18:22:44 -0300

    Update q2L2.py

--- Lista2/q2L2.py
+++ Lista2/q2L2.py
@@ -1,4 +1,7 @@
+#Inserindo o ano a ser analisado
 ano=int(input('Ano: '))
+
+#Verificando se o ano é bissexto e retornando o resultado
 if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
     print('É ano bissexto')
 else:
